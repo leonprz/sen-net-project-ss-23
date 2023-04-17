348cb5627850c0371c3babc780c071eff90be4cb
Neo Hornberger <neo.hornberger@smail.inf.h-brs.de>
2023-04-17 15:36:46 +0200

    Initial commit (pushing template project)

---
+++ src/main.cpp
@@ -0,0 +1,104 @@
+/**
+   @file main.cpp
+   @author Marco Roobi (marco.roobi@h-brs.de)
+   @brief Template
+          RAK4631
+   @version 1.0
+   @date 2023-03-01
+**/
+
+#define EXAMPLE_TEXT "Template\n"
+
+#include "main.h"
+
+// variable to keep a timestamp
+time_t timeout;
+// actual state of one led
+uint8_t led_state = HIGH;
+
+// forward declarations (needed for platformIO)
+void initLeds();
+void initSerial();
+
+/**
+ * @brief Application specific setup functions
+ *
+ */
+void setup(void)
+{
+  initLeds();
+  initSerial();
+
+  // ADD YOUR CODE HERE
+
+  // Keep the actual timestamp for the loop
+  timeout = millis();
+  // Setup finished...
+  MYLOG("SETUP", "Starting LOOP...");
+}
+
+/**
+ * @brief Application loop
+ *
+ */
+void loop(void)
+{
+  // Simple non-blocking loop
+  if ((millis() - timeout) > LOOP_TIMEOUT)
+  {
+    digitalWrite(LED_BLUE,  led_state);
+    digitalWrite(LED_GREEN,!led_state);
+    led_state = !led_state;
+    timeout = millis();
+  }
+}
+
+void initLeds()
+{
+  // Set GPIOs for LED's as Output
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+
+  // Set the GPIOs LOW (disable LEDs)
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_BLUE, LOW);
+}
+
+void initSerial()
+{
+  // Initialize Serial for debug output
+  digitalWrite(LED_GREEN, HIGH);
+  Serial.begin(115200);
+
+  // Initialize Serial for debug output
+  timeout = millis();
+  Serial.begin(115200);
+  while (!Serial)
+  {
+    if ((millis() - timeout) < 5000)
+    {
+      delay(100);
+    }
+    else
+    {
+      break;
+    }
+  }
+  // We should now have a serial interface...
+  // Add a short delay 15s before executing code
+  for(uint8_t i=0; i < 15;i++){
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.print("\n");
+  // disable the green LED
+  digitalWrite(LED_GREEN, LOW);
+  MYLOG("SETUP", "Starting...");
+
+  Serial.print("============================\n");
+  Serial.print("Hochschule Bonn-Rhein-Sieg\n");
+  Serial.print("Smart City: Sensorik und Netze\n");
+  Serial.print(EXAMPLE_TEXT);
+  Serial.printf("SW Version %d.%d.%d\n", SW_VERSION_1, SW_VERSION_2, SW_VERSION_3);
+  Serial.print("============================\n");
+}
---
+++ src/main.h
@@ -0,0 +1,62 @@
+/**
+   @file main.h
+   @author Marco Roobi (marco.roobi@h-brs.de)
+   @brief Header File
+          RAK4631
+   @version 1.0
+   @date 2023-03-01
+ **/
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#ifndef MY_DEBUG
+  #define MY_DEBUG 1 // to disable debug, set this to 0 (in PlatformIO this is done automatically, depending on the build target)
+#endif
+
+//***********************************************/
+//** Set the application firmware version here  */
+//** (in PlatformIO this is done automatically) */
+//***********************************************/
+// ; major version increase on API change / not backwards compatible
+#ifndef SW_VERSION_1
+  #define SW_VERSION_1 1
+#endif
+// ; minor version increase on API change / backward compatible
+#ifndef SW_VERSION_2
+  #define SW_VERSION_2 0
+#endif
+// ; patch version increase on bugfix, no affect on API
+#ifndef SW_VERSION_3
+  #define SW_VERSION_3 0
+#endif
+
+/** Include library's */
+#include <Arduino.h>
+#include <Adafruit_TinyUSB.h>
+/** Include TwoWire and SPI Interface */
+#include <SPI.h>
+#include <Wire.h>
+/** Include the SX126x-API */
+#include <SX126x-Arduino.h>
+/** Add your required includes after SX126x-Arduino.h */
+
+#if MY_DEBUG > 0
+    #define MYLOG(tag, ...)            \
+        do                             \
+        {                              \
+            if (tag)                   \
+                PRINTF("[%s] ", tag);  \
+            PRINTF(__VA_ARGS__);       \
+            PRINTF("\n");              \
+        } while (0)
+#else
+    #define MYLOG(...)
+#endif
+
+// ADD YOUR CODE HERE
+#define LOOP_TIMEOUT 1000
+#define SOIL_PIN WB_A1
+#define PIR_PIN WB_IO1
+
+#endif  // MAIN_H
