933bb5a46d031af84518404b58ad6417b2678f4d
lprinz2s <leon.prinz@smail.inf.h-brs.de>
2023-04-25 18:51:44 +0200

    Disabled gas heater in env_sensor.cpp

--- src/sensors/env_sensor.cpp
+++ src/sensors/env_sensor.cpp
@@ -15,4 +15,5 @@ void initEnvSensor()
 	env_sensor.setHumidityOversampling(BME680_OS_2X);
 	env_sensor.setPressureOversampling(BME680_OS_4X);
 	env_sensor.setIIRFilterSize(BME680_FILTER_SIZE_3);
+	env_sensor.setGasHeater(0, 0); // disables gas heater
 }
