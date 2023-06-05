a09f0afe0edc289d2f6b872c13d9841e37253737
Neo Hornberger <neo.hornberger@smail.inf.h-brs.de>
2023-06-05 15:26:46 +0200

    [MQTT-Client] upload requirements.txt to database server

--- mqtt-client/upload.sh
+++ mqtt-client/upload.sh
@@ -4,5 +4,6 @@ DIR="$(dirname "$0")"
 HOST='root@10.20.111.212'
 
 scp "$DIR"/sennet-mqtt.py "$HOST":/root/sennet-mqtt.py
+scp "$DIR"/requirements.txt "$HOST":/root/requirements.txt
 scp "$DIR"/sennet.service "$HOST":/etc/systemd/system/sennet.service
 ssh "$HOST" 'systemctl daemon-reload'
